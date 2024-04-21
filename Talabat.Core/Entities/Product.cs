5d27717a415454b64e5022947bf0c167f10714f9
moshahinnn <131661137+moshahinnn@users.noreply.github.com>
2024-04-21 19:20:30 +0200

    Part 08 Product Module - Entities

---
+++ Talabat.Core/Entities/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Entities
+{
+    public class Product:BaseEntity
+
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string PictureUrl { get; set; }
+
+        public decimal Price { get; set; }
+
+        //[ForeignKey(nameof(Product.Brand))]
+        public int BrandId { get; set; } // foregin Key Column => Product Barnd
+
+        public ProductBrand Brand { get; set; } // Navigational property [One]
+
+        public int CategoryId { get; set; } //foregin Key Column => ProductCategory
+
+        public ProductCategory Category { get; set; }// Navigational property [One]
+    }
+
+}
