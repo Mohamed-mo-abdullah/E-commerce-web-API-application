c8cdf35d47a9acab4d89c3ee288710c694366dfa
moshahinnn <131661137+moshahinnn@users.noreply.github.com>
2024-04-24 16:16:33 +0200

    Part 06 Product Picture Url Resolver

--- Talabat.APIs/Helpers/MappingProfiles.cs
+++ Talabat.APIs/Helpers/MappingProfiles.cs
@@ -1,17 +1,22 @@
 ﻿using AutoMapper;
 using Talabat.APIs.Dtos;
 using Talabat.Core.Entities;
+using static System.Net.WebRequestMethods;
 
 namespace Talabat.APIs.Helpers
 {
     public class MappingProfiles:Profile
     {
+        
 
         public MappingProfiles()
         {
-            CreateMap<Product, ProductToReturnDto>()
+           CreateMap<Product, ProductToReturnDto>()
                 .ForMember(d => d.Brand, o => o.MapFrom(s => s.Brand.Name))
-                .ForMember(d => d.Category, o => o.MapFrom(s => s.Category.Name));
+                .ForMember(d => d.Category, o => o.MapFrom(s => s.Category.Name))
+                //.ForMember(d => d.PictureUrl,o => o.MapFrom(S => https://localhost:7108S.PictureUrl));
+                .ForMember(d => d.PictureUrl, O => O.MapFrom<ProductPictureUrlResolver>());
         }
+
     }
 }
