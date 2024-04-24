a31cda5a68624a023354011be2b287c29930c331
moshahinnn <131661137+moshahinnn@users.noreply.github.com>
2024-04-24 16:36:30 +0200

    Part 07 UseStaticFiles Middleware

--- Talabat.APIs/Helpers/MappingProfiles.cs
+++ Talabat.APIs/Helpers/MappingProfiles.cs
@@ -7,15 +7,13 @@ namespace Talabat.APIs.Helpers
 {
     public class MappingProfiles:Profile
     {
-        
-
         public MappingProfiles()
         {
-           CreateMap<Product, ProductToReturnDto>()
-                .ForMember(d => d.Brand, o => o.MapFrom(s => s.Brand.Name))
-                .ForMember(d => d.Category, o => o.MapFrom(s => s.Category.Name))
-                //.ForMember(d => d.PictureUrl,o => o.MapFrom(S => https://localhost:7108S.PictureUrl));
-                .ForMember(d => d.PictureUrl, O => O.MapFrom<ProductPictureUrlResolver>());
+            _ = CreateMap<Product, ProductToReturnDto>()
+                 .ForMember(d => d.Brand, o => o.MapFrom(s => s.Brand.Name))
+                 .ForMember(d => d.Category, o => o.MapFrom(s => s.Category.Name))
+                 //.ForMember(d => d.PictureUrl,o => o.MapFrom(S => https://localhost:7108S.PictureUrl));
+                 .ForMember(d => d.PictureUrl, O => O.MapFrom<ProductPictureUrlResolver>());
         }
 
     }
