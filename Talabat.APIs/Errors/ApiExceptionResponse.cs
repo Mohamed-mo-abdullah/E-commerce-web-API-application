03614c89e8b00d0a90b353fcd0669ffec9b5fbc9
moshahinnn <131661137+moshahinnn@users.noreply.github.com>
2024-05-09 18:52:13 +0300

    Part 11 Server Error (Exception) Handling

---
+++ Talabat.APIs/Errors/ApiExceptionResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace Talabat.APIs.Errors
+{
+    public class ApiExceptionResponse:ApiResponse
+    {
+        public String? Details { get; set; }
+        public ApiExceptionResponse(int statusCode ,string? message = null,string? details = null)
+            :base(statusCode,message)
+        {
+            Details = details;
+        }
+
+
+    }
+}
---
+++ Talabat.APIs/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,49 @@
+﻿using System.Net;
+using System.Text.Json;
+using Talabat.APIs.Errors;
+
+namespace Talabat.APIs.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+        private readonly IWebHostEnvironment _env;
+
+        public ExceptionMiddleware( RequestDelegate next,ILogger<ExceptionMiddleware> logger,IWebHostEnvironment env)
+        {
+            _next = next;
+            _logger = logger;
+            _env = env;
+        }
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                //Take an Action with The Request
+
+                await _next.Invoke(httpContext); //Go To The Next Middleware
+
+                //Take an Action with The Response
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message); //Devolpment env
+                //log Exception (Database | Files) // production Env
+
+                httpContext.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+                httpContext.Response.ContentType = "application/Json";
+
+
+                var response = _env.IsDevelopment() ?
+                    new ApiExceptionResponse((int)HttpStatusCode.InternalServerError, ex.Message, ex.StackTrace.ToString())
+                    :
+                    new ApiExceptionResponse((int)HttpStatusCode.InternalServerError);
+                var options  = new JsonSerializerOptions() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+                var json = JsonSerializer.Serialize(response,options);
+
+                await httpContext.Response.WriteAsync(json);
+            }
+        }
+    }
+}
